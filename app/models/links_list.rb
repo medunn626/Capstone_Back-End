cef23bf5582dcd76f99fadc25bda0ad76ea9350e
medunn626 <MichaelE.Dunn@LibertyMutual.com>
2017-11-29 14:06:57 -0500

    Adds validation to links list for card user ID

--- app/models/links_list.rb
+++ app/models/links_list.rb
@@ -1,4 +1,14 @@
+class CardValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    # binding.pry
+    unless value.user_id == record.user_id
+      record.errors[attribute] << (options[:message] || "You don't have access to this card")
+    end
+  end
+end
+
 class LinksList < ApplicationRecord
   belongs_to :user
   belongs_to :card
+  validates :card, card: true
 end
